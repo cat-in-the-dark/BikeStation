579e1844c399c73f256bc720164b45581deb4dec
Kirill <kolbasisha@gmail.com>
2014-07-11 16:32:56 +0700

    Return bike works.

--- rpi/bluetooth_server.py
+++ rpi/bluetooth_server.py
@@ -8,13 +8,14 @@ from rpi.constants import SERVER_NAME, SERVER_UUID, BUFFER_SIZE, STATUS, OK, ON_
     GET_BIKES, ERROR, MESSAGE, DATA, HTTP_STATUS_UNAUTHORIZED, HTTP_STATUS_OK, GET_HAS_RENT, BIKE_ID, POST_START_RENT, \
     HTTP_STATUS_ACCEPTED, GATE_NUMBER, HTTP_STATUS_FORBIDDEN, POST_CLOSE_RENT, STATION_ID_KEY, \
     STATION_ID_VALUE
-from rpi.hardware import open_gate, get_opened_gate, lock_gate
+from rpi.hardware import open_gate, lock_gate
 
 
 class ClientThread(threading.Thread):
     def __init__(self, client_sock, client_info):
         threading.Thread.__init__(self)
         self._credentials = None
+        self._bike_id = None
         self.sock = client_sock
         self.info = client_info
         self._endpoint = WEB_SERVER_ADDR + ":{}".format(WEB_SERVER_PORT)
@@ -64,8 +65,9 @@ class ClientThread(threading.Thread):
 
                 if parsed_data[COMMAND] == SELECT_BIKE:
                     if parsed_data[BIKE_ID] is not None:
-                        bike_id = parsed_data[BIKE_ID]
-                        post_data = {BIKE_ID: bike_id}.update(self._credentials)
+                        self._bike_id = parsed_data[BIKE_ID]
+                        post_data = self._credentials
+                        post_data.update({BIKE_ID: self._bike_id})
                         select_bike_resp = requests.post(self._endpoint + POST_START_RENT, data=post_data).json()
                         log.debug(select_bike_resp)
                         if select_bike_resp[STATUS] == HTTP_STATUS_ACCEPTED:
@@ -77,9 +79,10 @@ class ClientThread(threading.Thread):
 
                 if parsed_data[COMMAND] == RETURN_BIKE:
                     # TODO: implement using GPIOs
-                    gate_id = get_opened_gate()
+                    gate_id = 1
                     if lock_gate(gate_id):
-                        post_data = {GATE_NUMBER: gate_id}.update(self._credentials)
+                        post_data = self._credentials
+                        post_data.update({GATE_NUMBER: gate_id, BIKE_ID: self._bike_id})
                         return_bike_resp = requests.post(self._endpoint + POST_CLOSE_RENT, data=post_data).json()
                         if return_bike_resp[STATUS] == HTTP_STATUS_ACCEPTED:
                             self.send(json.dumps({STATUS: OK}))
--- rpi/hardware.py
+++ rpi/hardware.py
@@ -1,26 +1,33 @@
+import RPi.GPIO as GPIO
 from rpi.config import log
 
-GATES = {1: True}
+_GPIO_INITED = False
+OPENED = False
+
+
+def init_gpio():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(5, GPIO.OUT)
+    global _GPIO_INITED
+    _GPIO_INITED = True
 
 
 def open_gate(gate_id):
     log.debug("Gate {} opened".format(gate_id - 1))
-    GATES.values()[gate_id - 1] = True
+    OPENED = True
+    # turn_led(True)
 
 
 def lock_gate(gate_id):
-    if GATES.values()[gate_id - 1]:
-        log.debug("Gate {} locked".format(gate_id))
-        GATES.values()[gate_id - 1] = False
-        return True
+    log.debug("Gate {} locked".format(gate_id))
+    # turn_led(False)
+    return True
+
+
+def turn_led(state):
+    if not _GPIO_INITED:
+        init_gpio()
+    if state:
+        GPIO.output(5, True)
     else:
-        return False
-
-
-def get_opened_gate():
-    gate_id = 1
-    for g in GATES.values():
-        if g:
-            return gate_id
-        else:
-            gate_id += 1
+        GPIO.output(5, False)
